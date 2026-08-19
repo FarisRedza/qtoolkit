107f4be07efbb9af2f8bb7638ddd68c4d188d6a0
Faris Redza <faris.redza1999@gmail.com>
2026-08-19 11:40:54 +0100

    added misc functions

---
+++ src/qkit/__init__.py
@@ -0,0 +1,11 @@
+from .misc_functions import (
+    binary_entropy,
+    fraction_to_dB,
+    dB_to_fraction
+)
+
+__all__ = [
+    'binary_entropy',
+    'fraction_to_dB',
+    'dB_to_fraction'
+]
---
+++ src/qkit/misc_functions.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def binary_entropy(x: float) -> float:
+    return -x * np.log2(x) - (1-x) * np.log2(1-x)
+
+def fraction_to_dB(x: float) -> float:
+    return -10 * np.log10(x)
+
+def dB_to_fraction(x: float) -> float:
+    return 10**(-x/10)
