6945fccdd7fb1a18f8c60207d30289fa51272acf
Faris Redza <faris.redza1999@gmail.com>
2026-09-08 11:34:47 +0000

    added subpackages as imports

--- src/qtoolkit/__init__.py
+++ src/qtoolkit/__init__.py
@@ -4,8 +4,18 @@ from .misc_functions import (
     dB_to_fraction
 )
 
+from . import (
+    polarisation,
+    qkd,
+    timetags,
+)
+
 __all__ = [
     'binary_entropy',
     'fraction_to_dB',
     'dB_to_fraction',
+
+    'polarisation',
+    'qkd',
+    'timetags',
 ]
