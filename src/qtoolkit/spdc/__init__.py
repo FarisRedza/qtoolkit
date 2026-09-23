b8dbf6daef32f6ea2eebf8b44f79dad2a94d976e
Faris Redza <faris.redza1999@gmail.com>
2026-09-23 15:25:17 +0000

    fix imports

--- src/qtoolkit/spdc/__init__.py
+++ src/qtoolkit/spdc/__init__.py
@@ -1,11 +1,25 @@
 from .materials import (
     RefractiveIndexAxis,
     NonlinearMaterial,
-    MgOLithiumNiobate
+    MgOLithiumNiobate,
+)
+
+from .phasematching import (
+    wavevector,
+    wavevector_mismatch,
+    poling_period_at_temperature,
+    poling_period_at_reference_temperature,
+    find_poling_period,
 )
 
 __all__ = [
     'RefractiveIndexAxis',
     'NonlinearMaterial',
     'MgOLithiumNiobate',
+
+    'wavevector',
+    'wavevector_mismatch',
+    'poling_period_at_temperature',
+    'poling_period_at_reference_temperature',
+    'find_poling_period',
 ]
