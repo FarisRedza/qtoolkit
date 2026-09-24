141422f9a0d522854fdaed0529b532eaa8034823
Faris Redza <faris.redza1999@gmail.com>
2026-09-24 15:38:26 +0000

    add spectral functions to init

--- src/qtoolkit/spdc/__init__.py
+++ src/qtoolkit/spdc/__init__.py
@@ -10,6 +10,19 @@ from .phasematching import (
     poling_period_at_temperature,
     poling_period_at_reference_temperature,
     find_poling_period,
+    find_phase_matching_temperature,
+    find_phase_matching_temperatures,
+    find_phase_matching_wavelengths,
+    find_phase_matching_wavelength_pairs,
+)
+
+from .spectral import (
+    angular_frequency,
+    wavelength_from_angular_frequency,
+    gaussian_pump_amplitude,
+    phase_matching_amplitude,
+    joint_spectral_amplitude,
+    joint_spectral_intensity,
 )
 
 __all__ = [
@@ -22,4 +35,15 @@ __all__ = [
     'poling_period_at_temperature',
     'poling_period_at_reference_temperature',
     'find_poling_period',
+    'find_phase_matching_temperature',
+    'find_phase_matching_temperatures',
+    'find_phase_matching_wavelengths',
+    'find_phase_matching_wavelength_pairs',
+
+    'angular_frequency',
+    'wavelength_from_angular_frequency',
+    'gaussian_pump_amplitude',
+    'phase_matching_amplitude',
+    'joint_spectral_amplitude',
+    'joint_spectral_intensity',
 ]
